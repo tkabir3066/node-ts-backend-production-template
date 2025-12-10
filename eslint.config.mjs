7c57e414183190f1fadc2161cdf9b95221af8ddd
Tutul <tutulkabir@gmail.com>
2025-12-10 21:29:56 +0530

    feat: Prettier setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,30 @@
 // @ts-nocheck
 
-import { defineConfig } from "eslint/config";
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
+import { defineConfig } from 'eslint/config'
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
 export default defineConfig([
-  {
-    ignores: ["dist/**", "build/**", "node_modules/**"],
-  },
-  {
-    languageOptions: {
-      parserOptions: {
-        project: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+    {
+        ignores: ['dist/**', 'build/**', 'node_modules/**']
     },
-  },
-  {
-    files: ["**/*.ts"],
-    extends: [
-      eslint.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked,
-      eslintConfigPrettier,
-    ],
-    rules: {
-      "no-console": "error",
-      "no-useless-catch": 0,
-      // quotes: ["error", "single", { allowTemplateLiterals: true }],
+    {
+        languageOptions: {
+            parserOptions: {
+                project: true,
+                tsconfigRootDir: import.meta.dirname
+            }
+        }
     },
-  },
-]);
+    {
+        files: ['**/*.ts'],
+        extends: [eslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked, eslintConfigPrettier],
+        rules: {
+            'no-console': 'error',
+            'no-useless-catch': 0,
+            quotes: ['error', 'single', { allowTemplateLiterals: true }]
+        }
+    }
+])
+
